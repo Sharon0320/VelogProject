dffd85fc248b0da4164119bae7acf465c1466cbc
이재용 <ijaeyong@ijaeyongs-MacBook-Pro.local>
2025-07-17 01:31:03 +0900

    [Chore] 프론트엔드 탭 제목 설명 추가

--- FrontEnd/app/layout.tsx
+++ FrontEnd/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Velog 자동 글 작성 도우미',
+  description: '글을 작성하면 AI로 적절한 태그를 추천해서 Velog애 게시물을 작성합니다.',
   generator: 'v0.dev',
 }
 
